39363e8944604df3ff70fa56c45993f8f2867c7d
heejunByun <hejeun22@gmail.com>
2022-11-26 23:08:06 +0900

    programmers day 2

---
+++ algorithm/src/programmers/level1/intSort.java
@@ -0,0 +1,24 @@
+package programmers.level1;
+
+import java.util.Arrays;
+
+/**
+ * @DisplayName("정수 내림차순으로 배치하기")
+ */
+public class intSort {
+    public static void main(String[] args) {
+        long n = 118372;
+        String str = String.valueOf(n);
+        String[] aryStr = str.split("");
+        Arrays.sort(aryStr); //오름차순
+        StringBuilder sb = new StringBuilder();
+        for (String aList : aryStr) {
+            // System.out.println("aList = " + aList);
+            sb.append(aList);
+            // System.out.println("sb = " + sb);
+        }
+        long answer = Long.parseLong(String.valueOf(sb.reverse())); // reverse 뒤집기
+        //873211
+        System.out.println("answer = " + answer);
+    }
+}
