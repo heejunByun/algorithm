225c2e80e1dc0cb2600c38ab261f39c056982060
heejunByun <hejeun22@gmail.com>
2022-11-27 22:23:08 +0900

    programmers day 3

---
+++ algorithm/src/programmers/level1/StarSquare.java
@@ -0,0 +1,18 @@
+package programmers.level1;
+
+public class StarSquare {
+    /**
+     * @DisplayName("직사각형 별찍기")
+     */
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        String star = "";
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ algorithm/src/programmers/level1/ThreeChange.java
@@ -0,0 +1,23 @@
+package programmers.level1;
+
+public class ThreeChange {
+    /**
+     * @DisplayName("3진법 뒤집기")
+     */
+    public static void main(String[] args) {
+//        String binaryString = Integer.toBinaryString(i); //2수
+//        String octalString = Integer.toOctalString(i);   //8진수
+//        String hexString = Integer.toHexString(i);       //16진수
+
+//        int binaryToDecimal = Integer.parseInt(binaryString, 2); // 2진수 -> 10진수
+//        int binaryToOctal = Integer.parseInt(octalString, 8); // 8진수 -> 10진수
+//        int binaryToHex = Integer.parseInt(hexString, 16); // 16진수 -> 10진수
+        int n = 125;
+        String strN = Integer.toString(n, 3);
+        StringBuilder sb = new StringBuilder(strN);
+        strN = sb.reverse().toString();
+        int answer = 0;
+        answer = Integer.parseInt(strN, 3);
+        System.out.println("answer = " + answer);
+    }
+}
