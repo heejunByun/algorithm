3d34bc9713fb89e725be9355c88d3c98ffcfaa9a
heejunByun <hejeun22@gmail.com>
2023-02-07 22:36:22 +0900

    programmers level2 day 6

---
+++ algorithm/src/programmers/level2/N개의최소공배수.java
@@ -0,0 +1,25 @@
+package programmers.level2;
+
+/**
+ * 두 수의 최소공배수(Least Common Multiple)란 입력된 두 수의 배수 중 공통이 되는 가장 작은 숫자를 의미합니다.
+ * 예를 들어 2와 7의 최소공배수는 14가 됩니다. 정의를 확장해서,
+ * n개의 수의 최소공배수는 n 개의 수들의 배수 중 공통이 되는 가장 작은 숫자가 됩니다.
+ *
+ * 제한 사항
+ * arr은 길이 1이상, 15이하인 배열입니다.
+ * arr의 원소는 100 이하인 자연수입니다.
+ * 입출력 예
+ * arr	        result
+ * [2,6,8,14]	168
+ * [1,2,3]	    6
+ */
+public class N개의최소공배수 {
+    public static void main(String[] args) {
+        solution(new int[] {2,6,8,14});
+    }
+
+    public static int solution(int[] arr) {
+        int answer = 0;
+        return answer;
+    }
+}
