8cff6c5b8450f177e91c98b3b23a40cff902e98f
heejunByun <hejeun22@gmail.com>
2023-02-09 22:53:40 +0900

    programmers level2 day 7

--- algorithm/src/programmers/level1/최대공약수와최소공배수.java
+++ algorithm/src/programmers/level1/최대공약수와최소공배수.java
@@ -5,8 +5,8 @@ public class 최대공약수와최소공배수 {
      * @DisplayName("최대공약수와 최소공배수")
      */
     public static void main(String[] args) {
-        int n = 5;
-        int m = 7;
+        int n = 2;
+        int m = 6;
 
         int max = Math.max(n, m);
         int min = Math.min(n, m);
@@ -14,7 +14,7 @@ public class 최대공약수와최소공배수 {
         //최대 공약수
         //유클리드 호재법 알고리즘 => 최대공약수 구할 때 사용
         //2개의 자연수 a,b 에 대해서 a를 b로 나눈 나머지를 r 이라하면 (단,a>b) a와 b의 최대공약수는 b와 r의 최대공약수와 같다..
-        while (min != 0) {
+        while (max % min != 0) {
             int r = max % min;
             max = min;
             min = r;
@@ -32,7 +32,7 @@ public class 최대공약수와최소공배수 {
          *     public int[] gcdlcm(int a, int b) {
          *         int[] answer = new int[2];
          *
-         *           answer[0] = gcd(a,b);
+         *         answer[0] = gcd(a,b);
          *         answer[1] = (a*b)/answer[0];
          *         return answer;
          *     }
--- algorithm/src/programmers/level2/N개의최소공배수.java
+++ algorithm/src/programmers/level2/N개의최소공배수.java
@@ -20,6 +20,25 @@ public class N개의최소공배수 {
 
     public static int solution(int[] arr) {
         int answer = 0;
+        answer = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            int gcd = gcd(answer, arr[i]);
+            answer = answer * arr[i] / gcd; // answer 가 두 수의 비교하는 최소공배수가 된다.
+        }
+        System.out.println("answer = " + answer);
         return answer;
+    }
+
+    //최대공약수 구하는 함수 (유클리드 호제법)
+    public static int gcd(int a, int b){
+        int max = Math.max(a,b);
+        int min = Math.min(a,b);
+        while(min != 0){
+            int r = max % min;
+            max = min;
+            min = r;
+        }
+        return min;
     }
 }
